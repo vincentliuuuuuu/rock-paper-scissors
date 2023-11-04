b05ac63abb9c2e4e54098fe37a74202421c1b736
vincentliuuuuuu <alizarinaurora@gmail.com>
2023-11-03 19:49:42 -0700

    Added a rock, paper, scissors game

---
+++ index.js
@@ -0,0 +1,95 @@
+const Choices = Object.freeze({
+  ROCK: 0,
+  PAPER: 1,
+  SCISSORS: 2,
+})
+
+const Results = Object.freeze({
+  PLAYERWIN: 1,
+  TIE: 0,
+  PLAYERLOSE: -1,
+})
+
+const getComputerChoice = () => Math.floor(Math.random() * 3);
+
+const getPlayerChoice = () => {
+  let result = -1;
+  
+  while (result < 0) {
+    const playerChoice = prompt("Please enter your choice of rock, paper, or scissors.");
+    result = parseUserInput(playerChoice);
+    if (result < 0) {
+      alert("Not a valid choice. Try again!")
+    }
+  }
+  return result;
+}
+
+const parseUserInput = (userInput) => {
+  const lowerUserInput = userInput.toLowerCase();
+  switch (lowerUserInput) {
+    case 'rock':
+      return Choices.ROCK;
+    case 'paper':
+      return Choices.PAPER;
+    case 'scissors':
+      return Choices.SCISSORS;
+    default:
+      return -1;
+  }
+}
+
+const playSingleRound = () => {
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
+  //console.log(`Player: ${playerSelection} Computer: ${computerSelection}`);
+  
+  if (playerSelection === computerSelection) {
+    return Results.TIE;
+  }
+  
+  if (playerSelection === Choices.ROCK && computerSelection === Choices.SCISSORS ||
+      playerSelection === Choices.PAPER && computerSelection === Choices.ROCK ||
+      playerSelection === Choices.SCISSORS && computerSelection === Choices.PAPER) {
+    return Results.PLAYERWIN;
+  }
+
+  return Results.PLAYERLOSE;
+}
+
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  for (i = 0; i < 5; i++) {
+    const result = playSingleRound();
+    switch (result) {
+      case Results.PLAYERWIN:
+        playerScore++;
+        console.log("You won!");
+        break;
+      case Results.PLAYERLOSE:
+        computerScore++;
+        console.log("You lost!");
+        break;
+      default:
+        console.log("It's a tie!");
+        break;
+    }
+    
+    let scoreMessage = `Player score: ${playerScore} Computer Score ${computerScore}`;
+    console.log(scoreMessage);
+  }
+  console.log();
+  if (playerScore > computerScore) {
+    console.log('You won!');
+  } else if (playerScore === computerScore) {
+    console.log("It's a tie!");
+  } else {
+    console.log("YOu lost!");
+  }
+}
+
+game();
+
+//const playRound = (playerSelection, computerSelection) =>
